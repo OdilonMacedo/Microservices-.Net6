bb2cac16013f646019fbe45f7ce0727622f11297
OdilonMacedo <odilon.macedo@sixconsulta.com.br>
2022-11-07 14:56:36 -0300

    FirstArchitecture

---
+++ GeekShopping/GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Model.Context
+{
+    public class MySQLContext : DbContext
+    {
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 2,
+                Name = "Name",
+                Price = new decimal(69.9),
+                Description = "Description",
+                ImageURL = "",
+                CategoryName = "Skirt"
+            });
+
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 4,
+                Name = "Caruzo's hair",
+                Price = new decimal(0.0),
+                Description = "Actually free, nobody want's that shit",
+                ImageURL = "Caruzo Likes it",
+                CategoryName = "Sausage Hair"
+            });
+        }
+    }
+}
---
+++ GeekShopping/GeekShopping.ProductAPI/Model/Product.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using GeekShopping.ProductAPI.Model.Base;
+
+namespace GeekShopping.ProductAPI.Model
+{
+    [Table("product")]
+    public class Product : BaseEntity
+    {
+        [Column("Name")]
+        [Required]
+        [StringLength(150)]
+        public string Name { get; set; }
+
+        [Column("Price")]
+        [Required]
+        [Range(10,10000)]
+        public decimal Price{ get; set; }
+
+        [StringLength(300)]
+        [Column("description")]
+        public string Description { get; set; }
+
+        [Column("category_name")]
+        [StringLength(50)]
+        public string CategoryName { get; set; }
+
+        [Column("image_url")]
+        [StringLength(300)]
+        public string ImageURL { get; set; }
+
+
+    }
+}
